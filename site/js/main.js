db609d8b9563aabd8b651f4afb944d1bb35c693d
Dillon Grove <dillongrove@gmail.com>
2013-01-17 15:06:41 -0500

    reorganized basic particle system code to be organized around an application object

--- site/js/main.js
+++ site/js/main.js
@@ -1,89 +1,104 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    new LaserCommand("canvas");
+})
 
-  var resizeCanvas = function(){
-    console.log("resize");
-    var canvas = document.getElementById("canvas");
+function LaserCommand(canvasName) {
+    this.resizeCanvas(canvasName);
+    this.init(canvasName);
+}
+
+LaserCommand.prototype.init = function(canvasName){
+    // constants
+    this.bgColor = [0,0,0];
+    this.camPosition = [0,0,0];
+    this.camLookPoint = [0,0,-1];
+    this.particlePosition = [0,0,-20];
+    this.particleMinVelocity = [-4,-4,-4];
+    this.particleMaxVelocity = [-2,2,2];
+    this.particleMinLifetime = .5;
+    this.particleMaxLifetime = 1;
+    this.particleMinColor = [0.8,0.4,0.4,0.5];
+    this.particleMaxColor = [1,0.6,0.6,1];
+    this.particleMinSize = .05;
+    this.particleMaxSize = .1;
+    this.particleAcceleration = [0,-50,0];
+    this.particleEmitRate = 100;
+    this.totalParticles = 50;
+
+
+    this.initScene(canvasName);
+    this.initCamera(scn);
+    this.initParticleSystem();
+
+    scn.setCamera(this.cam); // add the camera to the scene
+    scn.startScene(); // start the scene
+    scn.addObjectToScene(psys); //add the particle system to the scene
+}
+
+LaserCommand.prototype.resizeCanvas = function(canvasName){
+    var canvas = document.getElementById(canvasName);
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
-  }
+}
 
-  $(window).resize(function() {
-    resizeCanvas();
-  });
-
-  //Particle Systems Tutorial
-  var psys;
-  var cam;
-
-  resizeCanvas();
-
-  c3dl.addMainCallBack(canvasMain, "canvas");
-
-  // The program main
-  function canvasMain(canvasName)
-  {
-    // Create new c3dl.Scene object
+LaserCommand.prototype.initScene = function(canvasName){
     scn = new c3dl.Scene();
-
     scn.setCanvasTag(canvasName);
 
-    // Create GL context
+    // create GL context
     renderer = new c3dl.WebGL();
-    //set the background of the scene
-    scn.setBackgroundColor([0,0,0]);
 
-    // Attach renderer to the scene
+    scn.setBackgroundColor(this.bgColor);
+
+    // attach renderer to the scene
     scn.setRenderer(renderer);
     scn.init();
-    //create a camera
+
+    this.scn = scn;
+}
+
+LaserCommand.prototype.initCamera = function(){
     cam = new c3dl.FreeCamera();
-        
-    // Place the camera.
-    cam.setPosition([0, 0, 0]);
+    cam.setPosition(this.camPosition); // place the camera.
+    cam.setLookAtPoint(this.camLookPoint); // point camera directly down z-axis
+    this.cam = cam;
+}
 
-    // Point the camera.
-    // Here it is pointed directly down the Z-axis..
-    cam.setLookAtPoint([0, 0, -1]);
-
-    // Add the camera to the scene
-    scn.setCamera(cam);
-
-    // Start the scene
-    scn.startScene();
-
-    //This is the new code for the particle system
-    //add the particle system
+LaserCommand.prototype.initParticleSystem = function(){
     psys = new c3dl.ParticleSystem();
-    //position it in the scene, in full view of the camera
-    psys.setPosition([0,0,-20]);
+    psys.setPosition(this.particlePosition);
+
     //set the initial range of velocities possible for each particle
-    psys.setMinVelocity([-4,-4,-4]);
-    psys.setMaxVelocity([-2,2,2]);
+    psys.setMinVelocity(this.particleMinVelocity);
+    psys.setMaxVelocity(this.particleMaxVelocity);
+
     //set the range of time (in seconds) that a particle can last
-    psys.setMinLifetime(0.5);
-    psys.setMaxLifetime(1);
+    psys.setMinLifetime(this.particleMinLifetime);
+    psys.setMaxLifetime(this.particleMaxLifetime);
+
     //Set the range of colours for particles
-    psys.setMinColor([0.8,0.4,0.4,0.5]);
-    psys.setMaxColor([1,0.6,0.6,1]);
+    psys.setMinColor(this.particleMinColor);
+    psys.setMaxColor(this.particleMaxColor);
+
     //set range of sizes for particle
-    psys.setMinSize(0.05);
-    psys.setMaxSize(0.1);
+    psys.setMinSize(this.particleMinSize);
+    psys.setMaxSize(this.particleMaxSize);
+
     //specify how overlapping particles will be rendered
     psys.setSrcBlend(c3dl.ONE);
     psys.setDstBlend(c3dl.DST_ALPHA);
+
     //set the texture the particles will use
     //psys.setTexture("flare.png");
+
     //Set the acceleration that will be applied to every particle
-    psys.setAcceleration([0,-50,0]);
+    psys.setAcceleration(this.particleAcceleration);
+
     //Set how many particles the system will emit every second
-    psys.setEmitRate(100);
+    psys.setEmitRate(this.particleEmitRate);
+
     //Set the total number of particles available to the system
-    psys.init(50);
+    psys.init(this.totalParticles);
 
-    //add the particle system to the scene
-    scn.addObjectToScene(psys);
-
-  //END OF NEW CODE
-  }
-
- });
+    this.psys = psys;
+}
